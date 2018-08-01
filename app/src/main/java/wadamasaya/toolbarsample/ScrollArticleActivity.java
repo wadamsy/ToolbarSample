dbd5701642711257822d155f3942cc4cdcdcfc6c
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 12:42:45 +0900

    init

---
+++ app/src/main/java/wadamasaya/toolbarsample/ScrollArticleActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.toolbarsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_article);
+    }
+}
