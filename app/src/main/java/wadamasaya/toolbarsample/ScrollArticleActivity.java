4207729d2ee5724c2e5ac1af594d11bbcc99640a
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 20:33:10 +0900

    test

--- app/src/main/java/wadamasaya/toolbarsample/ScrollArticleActivity.java
+++ app/src/main/java/wadamasaya/toolbarsample/ScrollArticleActivity.java
@@ -1,7 +1,11 @@
 package wadamasaya.toolbarsample;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ScrollArticleActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class ScrollArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_article);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle(R.string.toolbar_title);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setSubtitleTextColor(Color.LTGRAY);
+        setSupportActionBar(toolbar);
+
+
     }
+
+
 }
